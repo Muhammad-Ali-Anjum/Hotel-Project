189199fbd53875e0a30d972f3990f55288e1692a
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-04-15 01:06:36 +0500

    add Bbarchart

--- Hotels/Front End/src/componsent/Dashbaord/Graph/BarChart .jsx
+++ Hotels/Front End/src/componsent/Dashbaord/Graph/BarChart .jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
 
-const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
-const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490.3422];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300,2323];
 const xLabels = [
   'Page A',
   'Page B',
@@ -11,6 +11,7 @@ const xLabels = [
   'Page E',
   'Page F',
   'Page G',
+  'Page H',
 ];
 
 export default function SimpleBarChart() {
@@ -22,6 +23,7 @@ export default function SimpleBarChart() {
         { data: pData, label: 'pv', id: 'pvId' },
         { data: uData, label: 'uv', id: 'uvId' },
       ]}
+
       xAxis={[{ data: xLabels, scaleType: 'band' }]}
     />
   );
