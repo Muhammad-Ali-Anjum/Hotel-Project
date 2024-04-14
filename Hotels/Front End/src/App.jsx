3a4ba693ad4b0e5497e063765b77ba32d4288c8b
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-04-15 01:03:36 +0500

    add Bbarchart

--- Hotels/Front End/src/App.jsx
+++ Hotels/Front End/src/App.jsx
@@ -1,112 +1,20 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import AddHomeIcon from '@mui/icons-material/AddHome';
-import HotelIcon from '@mui/icons-material/Hotel';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import PersonIcon from '@mui/icons-material/Person';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Navbar, Container, Nav, Button, ListGroup } from 'react-bootstrap';
+import { Drawer, DrawerHeader } from '@mui/material';
+import { FaBars, FaChevronLeft, FaChevronRight, FaHome, FaHotel, FaShoppingCart, FaUser } from 'react-icons/fa';
 import Dashbaord from './componsent/Dashbaord/Dashbaord';
 import Hotels from './componsent/Hotels/Hotels';
 import Room from './componsent/Rooms/Room';
 import LoginForm from './componsent/LoginRegister/Login';
 import RegisterForm from './componsent/LoginRegister/Register';
-import Button from '@mui/material/Button';
-import BookIcon from '@mui/icons-material/Book';
 import Booking from './componsent/Booking/Booking';
 import User from './componsent/users/User';
-import BedIcon from '@mui/icons-material/Bed';
-import HomeWorkIcon from '@mui/icons-material/HomeWork';
-
-
-// Import your components here
-// ~
 
 const drawerWidth = 240;
 
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
-
-export default function MiniDrawer() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+const MiniDrawer = () => {
+  const [open, setOpen] = useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -118,89 +26,53 @@ export default function MiniDrawer() {
 
   return (
     <Router>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="fixed" open={open}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              onClick={handleDrawerOpen}
-              edge="start"
-              sx={{
-                marginRight: 5,
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h4" noWrap component="div">
-              Rinor 
-            </Typography>
-            <div style={{ marginLeft: 'auto' }}>
-                <Button
-  color="inherit"
-  component={Link}
-  to="/login"
-  sx={{ '&:hover': { backgroundColor: 'yellow', color: 'black' } }}
->
-  Login
-</Button>
-<Button
-  color="inherit"
-  component={Link}
-  to="/register"
-  sx={{ '&:hover': { backgroundColor: 'yellow', color: 'black' } }}
->
-  Register
-</Button>
-                </div>
-          </Toolbar>
-      
-        </AppBar>
+      <div style={{ display: 'flex' }}>
+        <Navbar bg="dark" variant="dark" expand="lg" open={open}>
+          <Container>
+            <Navbar.Brand>Rinor</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleDrawerOpen}>
+              <FaBars />
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Button as={Link} to="/login" variant="light">Login</Button>
+                <Button as={Link} to="/register" variant="light">Register</Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Drawer variant="permanent" open={open}>
           <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
+            <FaChevronLeft onClick={handleDrawerClose} style={{ cursor: 'pointer' }} />
           </DrawerHeader>
-          <Divider />
-          <List>
-            {/* Define routes and navigate to respective components */}
+          <ListGroup>
             {[
-              
-              { title: 'Dashboard', icon: <AddHomeIcon />, path: '/dashboard' },
-              { title: 'Hotels', icon: <HomeWorkIcon />, path: '/hotels' },
-              { title: 'Room', icon: <BedIcon />, path: '/room' },
-              { title: 'Booking', icon: <BookIcon />, path: '/booking' },
-              { title: 'User', icon: <PersonIcon />, path: '/user' },
+              { title: 'Dashboard', icon: <FaHome />, path: '/dashboard' },
+              { title: 'Hotels', icon: <FaHotel />, path: '/hotels' },
+              { title: 'Room', icon: <FaHotel />, path: '/room' },
+              { title: 'Booking', icon: <FaShoppingCart />, path: '/booking' },
+              { title: 'User', icon: <FaUser />, path: '/user' },
             ].map((item) => (
-              <ListItem key={item.title} disablePadding sx={{ display: 'block' }}>
-                <ListItemButton component={Link} to={item.path}>
-                  <ListItemIcon>
-                    {item.icon}
-                  </ListItemIcon>
-                  <ListItemText primary={item.title} />
-                </ListItemButton>
-              </ListItem>
+              <Link key={item.title} to={item.path} className="list-group-item list-group-item-action" onClick={handleDrawerClose}>
+                {item.icon} {item.title}
+              </Link>
             ))}
-          </List>
-          <Divider />
+          </ListGroup>
         </Drawer>
-        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <DrawerHeader />
+        <main style={{ flexGrow: 1 }}>
           <Routes>
-            {/* Define routes for components */}
-            <Route path="/dashboard"   element={<Dashbaord/>} />
-            <Route path="/hotels" element={<Hotels/>} />
-            <Route path="/room" element={<Room/>} />
-            <Route path="/booking" element={<Booking/>} />
-            <Route path="/user" element={<User/>} />
+            <Route path="/dashboard" element={<Dashbaord />} />
+            <Route path="/hotels" element={<Hotels />} />
+            <Route path="/room" element={<Room />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/user" element={<User />} />
             <Route path="/login" element={<LoginForm />} />
-  <Route path="/register" element={<RegisterForm />} />
+            <Route path="/register" element={<RegisterForm />} />
           </Routes>
-        </Box>
-      </Box>
+        </main>
+      </div>
     </Router>
   );
-}
+};
+
+export default MiniDrawer;
