8deec9c3ef37df8e5e28c1e2c8a6e68018d2f348
Muhammad Ali Anjum <anjumbalgharii@gmail.com>
2024-04-14 23:20:02 +0500

    add pie chart

--- Hotels/Front End/src/componsent/Dashbaord/Dashbaord.jsx
+++ Hotels/Front End/src/componsent/Dashbaord/Dashbaord.jsx
@@ -1,10 +1,12 @@
 // import PieChart from './PieChart'
 import BarChart from './Graph/BarChart ';
+import BasicPie from './Graph/PieChart';
 const Dashboard = () => {
   return (
     <>
      {/* <PieChart/> */}
      <BarChart/>
+     <BasicPie/>
     </>
   );
 };
